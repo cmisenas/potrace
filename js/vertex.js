2237ed19b603449b9e8a5bfc5118d631ac2f50e8
cmisenas <acolyte.camille@gmail.com>
2013-08-13 12:05:35 -0400

    Created a Vertex object that has methods to get its 4 neighboring pixel and check if it is an edge

---
+++ js/vertex.js
@@ -0,0 +1,31 @@
+;(function(exports) {
+  
+  /*
+   * Iterate through the canvas image vertex (NW point)
+   * Check each one if it is an edge based on the condition that all four neighbors are not equal in value
+   */
+
+  function Vertex(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  Vertex.prototype.getNeighbors = function(width, height, coords) {
+    var neighbors = {};
+    neighbors.nw = coordsToIndex(width, height, {x: coords.x - 1, y: coords.y - 1});
+    neighbors.ne = coordsToIndex(width, height, {x: coords.x, y: coords.y - 1});
+    neighbors.sw = coordsToIndex(width, height, {x: coords.x - 1, y: coords.y});
+    neighbors.se = coordsToIndex(width, height, {x: coords.x, y: coords.y});
+    return neighbors;
+  }
+
+  Vertex.prototype.checkIfEdge = function(imgData) {
+    var neighbors = getNeighbors(imgData.width,
+                                 imgData.height,
+                                 {x: thix.x, y: this.y});
+    return (imgData.data[neighbors.nw] ===  imgData.data[neighbors.ne] &&
+            imgData.data[neighbors.sw] ===  imgData.data[neighbors.se] &&
+            imgData.data[neighbors.nw] ===  imgData.data[neighbors.ne]) ? true : false;
+  }
+
+}(this));
