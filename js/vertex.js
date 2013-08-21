b9d1b925aace2fd408f7df0e9666110ea79fd2b6
cmisenas <acolyte.camille@gmail.com>
2013-08-21 16:37:19 -0400

    Wrote tests for VertexFinder and Vertex object VertexFinder and Vertex object now passes :)

--- js/vertex.js
+++ js/vertex.js
@@ -1,39 +1,96 @@
 ;(function(exports) {
-  
+
   /*
    * Iterate through the canvas image vertex (NW point)
    * Check each one if it is an edge based on the condition that all four neighbors are not equal in value
    */
-  
+
   //this object will be responsible for finding all vertices given a canvas image
-  function VertexFinder(canvas) {
+  function VertexFinder (imgData) {
+    if (typeof imgData == 'undefined') {
+      throw new Error('No image data passed!');
+    }
+    this.imgData = imgData;
+    this.allVertices = [];
   }
 
-  VertexFinder.prototype.getAllVertices = function() {
-  }
+  VertexFinder.prototype.addVertex = function (coords) {
+    return typeof this.allVertices.push(new Vertex(coords.x, coords.y)) === 'number' ? true : false;
+  };
+
+  VertexFinder.prototype.findAllVertices = function () {
+    var vertex;
+		for (var y = 0, maxH = this.imgData.height; y < maxH; y++) {
+			for (var x = 0, maxW = this.imgData.width; x < maxW; x++) {
+        vertex = new Vertex(x, y);
+        if (vertex.checkIfEdge(this.imgData) === true) {
+          this.addVertex({x: x, y: y});
+        }
+			}
+		}
+  };
+
+  VertexFinder.prototype.getAllVertices = function (coords) {
+    return this.allVertices;
+  };
 
   //this object will be handling individual vertex particularly determining if a vertex is an edge or not
   function Vertex(x, y) {
+    if (typeof x == 'undefined' || typeof y == 'undefined') {
+      throw new Error('No x and y passed!');
+    }
     this.x = x;
     this.y = y;
   }
 
-  Vertex.prototype.getNeighbors = function(width, height, coords) {
+  Vertex.prototype.getNeighbors = function (width, height) {
+    this.width = width;
+    this.height = height;
+
     var neighbors = {};
-    neighbors.nw = coordsToIndex(width, height, {x: coords.x - 1, y: coords.y - 1});
-    neighbors.ne = coordsToIndex(width, height, {x: coords.x, y: coords.y - 1});
-    neighbors.sw = coordsToIndex(width, height, {x: coords.x - 1, y: coords.y});
-    neighbors.se = coordsToIndex(width, height, {x: coords.x, y: coords.y});
+    var checkedIfBorder = this.checkIfBorder(width, height);
+
+    neighbors.nw = checkedIfBorder.top || checkedIfBorder.left ? null : {x: this.x - 1, y: this.y - 1};
+    neighbors.ne = checkedIfBorder.top || checkedIfBorder.right ? null : {x: this.x, y: this.y - 1};
+    neighbors.sw = checkedIfBorder.bottom || checkedIfBorder.left ? null : {x: this.x - 1, y: this.y};
+    neighbors.se = checkedIfBorder.bottom || checkedIfBorder.right ? null : {x: this.x, y: this.y};
     return neighbors;
+  };
+
+  Vertex.prototype.checkIfBorder = function (width, height) {
+    var borders = {};
+    borders.top = this.y === 0 ? true : false;
+    borders.bottom = this.y === height ? true : false;
+    borders.left = this.x === 0 ? true : false;
+    borders.right = this.x === width ? true : false;
+    return borders;
+  };
+
+  Vertex.prototype.checkIfEdge = function (imgData) {
+    var neighbors = this.getNeighbors(imgData.width, imgData.height),
+        currPixel, otherPixel;
+
+    //the horror D:
+    for (currPixel in neighbors) {
+      if (neighbors.hasOwnProperty(currPixel) && neighbors[currPixel] !== null) {
+        for (otherPixel in neighbors) {
+          if (neighbors.hasOwnProperty(otherPixel) && neighbors[otherPixel] !== null) {
+            if (imgData.data[coordsToIndex(neighbors[currPixel], imgData.width)] !== imgData.data[coordsToIndex(neighbors[otherPixel], imgData.width)]) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+
+    return false;
+  };
+  
+  function coordsToIndex (coords, width) {
+    return (coords.x * 4) + (coords.y * width * 4);  
   }
 
-  Vertex.prototype.checkIfEdge = function(imgData) {
-    var neighbors = getNeighbors(imgData.width,
-                                 imgData.height,
-                                 {x: thix.x, y: this.y});
-    return (imgData.data[neighbors.nw] ===  imgData.data[neighbors.ne] &&
-            imgData.data[neighbors.sw] ===  imgData.data[neighbors.se] &&
-            imgData.data[neighbors.nw] ===  imgData.data[neighbors.ne]) ? true : false;
-  }
+  exports.VertexFinder = VertexFinder;
+  exports.Vertex = Vertex;
 
 }(this));
--- test/vertex.js
+++ test/vertex.js
@@ -1,6 +1,178 @@
-var assert = require('assert');
+var assert = require('assert'),
+    VertexFinder = require('../js/vertex').VertexFinder;
+Vertex = require('../js/vertex').Vertex;
 
-describe('Test', function() {
+var objTests = {
+      vertexFinder: null,
+      vertex: null
+    },
+    blankImgData = {},
+    sqrImgData = {};
+
+/*
+ * Vertex Finder is responsible for gathering all vertices
+ * It is given an image data and iterates through each of that
+ * It does not mind which vertices are connected or not
+ */
+
+describe('Vertex Finder', function () {
+  setup(function () {
+    blankImgData.height = 3;
+    blankImgData.width = 3;
+    //4 elements for each pixel for r, g, b, a values
+    blankImgData.data = new Uint8ClampedArray(blankImgData.height * blankImgData.width * 4);
+    for (var i = 0; i < blankImgData.data.length; i++) {
+      blankImgData.data[i] = 255;
+    }
+
+    sqrImgData.height = 4;
+    sqrImgData.width = 4;
+    //4 elements for each pixel for r, g, b, a values
+    sqrImgData.data = new Uint8ClampedArray(sqrImgData.height * sqrImgData.width * 4);
+    for (var j = 0; j < sqrImgData.data.length; j++) {
+      sqrImgData.data[j] = 255;
+    }
+    //Make one pixel black so there will be an existing 'edge' 
+    sqrImgData.data[20] = 0;
+    sqrImgData.data[21] = 0;
+    sqrImgData.data[22] = 0;
+  });
+
+  beforeEach(function () {
+    delete objTests.vertexFinder;
+  });
+
+  it('should be initialized given image data', function () {
+    objTests.vertexFinder = new VertexFinder(blankImgData);
+
+    assert.equal(objTests.vertexFinder instanceof VertexFinder, true);
+  });
+
+  it('should throw an error if initialized without image data', function () {
+    var errorMsg = new Error('No image data passed!');
+    assert.throws(function() {
+      objTests.vertexFinder = new VertexFinder();
+    }, errorMsg.message);
+  });
+
+  it('should add a vertex to its internal array of all vertices', function () {
+    objTests.vertexFinder = new VertexFinder(blankImgData);
+
+    assert.equal(objTests.vertexFinder.addVertex({x: 1, y: 1}), true);
+    assert.equal(objTests.vertexFinder.getAllVertices().length, 1);
+  });
   
+  it('should be able to find all vertices that are edges', function () {
+    objTests.vertexFinder = new VertexFinder(sqrImgData);
+    objTests.vertexFinder.findAllVertices();
+    var allVertices = objTests.vertexFinder.getAllVertices();
+
+    assert.equal(allVertices.length, 4);
+  });
+
+  it('should return groups of vertex', function () {
+    objTests.vertexFinder = new VertexFinder(blankImgData);
+    objTests.vertexFinder.addVertex({x: 0, y: 0});
+    objTests.vertexFinder.addVertex({x: 1, y: 0});
+    objTests.vertexFinder.addVertex({x: 1, y: 1});
+
+    assert.equal(objTests.vertexFinder.getAllVertices().length, 3);
+    assert.equal(objTests.vertexFinder.getAllVertices().join(''), '[object Object][object Object][object Object]');
+  });
+
+});
+
+/*
+ * Vertex is just an object created to
+ * check if a given coordinate of a vertex is in fact an edge
+ * and to handle vertices that are on the boundaries of an image
+ * it also has a method to get its 4 neighboring pixels
+ * and its 4 neighboring vertex
+ */
+
+describe('Vertex', function() {
+  setup(function () {
+    blankImgData.height = 3;
+    blankImgData.width = 3;
+    //4 elements for each pixel for r, g, b, a values
+    blankImgData.data = new Uint8ClampedArray(blankImgData.height * blankImgData.width * 4);
+    for (var i = 0; i < blankImgData.data.length; i++) {
+      blankImgData.data[i] = 255;
+    }
+
+    sqrImgData.height = 4;
+    sqrImgData.width = 4;
+    //4 elements for each pixel for r, g, b, a values
+    sqrImgData.data = new Uint8ClampedArray(sqrImgData.height * sqrImgData.width * 4);
+    for (var j = 0; j < sqrImgData.data.length; j++) {
+      sqrImgData.data[j] = 255;
+    }
+    //Make one pixel black so there will be an existing 'edge' 
+    sqrImgData.data[20] = 0;
+    sqrImgData.data[21] = 0;
+    sqrImgData.data[22] = 0;
+  });
+
+  beforeEach(function () {
+    delete objTests.vertex;
+  });
+
+  it('should be able to initialize with x and y passed', function() {
+    objTests.vertex = new Vertex(1, 1);
+    assert.equal(objTests.vertex instanceof Vertex, true);
+  });
+
+  it('should throw error when initialized without x and y passed', function() {
+    var errorMsg = new Error('No x and y passed!');
+    assert.throws(function() {
+      objTests.vertex = new Vertex();
+    }, errorMsg.message);
+  });
+
+  it('should be able to get its neighboring pixels correctly', function() {
+    objTests.vertex = new Vertex(1, 1);
+    var neighbors = objTests.vertex.getNeighbors(3, 3);
+
+    assert.equal(neighbors.nw.x, 0);
+    assert.equal(neighbors.nw.y, 0);
+    assert.equal(neighbors.ne.x, 1);
+    assert.equal(neighbors.ne.y, 0);
+    assert.equal(neighbors.sw.x, 0);
+    assert.equal(neighbors.sw.y, 1);
+    assert.equal(neighbors.se.x, 1);
+    assert.equal(neighbors.se.y, 1);
+  });
+
+  it('should handle vertices that are boundaries of the image', function() {
+    var vertex1 = new Vertex(0, 0);
+    var vertex2 = new Vertex(0, 1);
+    var neighbors1 = vertex1.getNeighbors(3, 3);
+    var neighbors2 = vertex2.getNeighbors(3, 3);
+
+    assert.equal(neighbors1.nw, null);
+    assert.equal(neighbors1.ne, null);
+    assert.equal(neighbors1.sw, null);
+    assert.equal(neighbors1.se.x, 0);
+    assert.equal(neighbors1.se.y, 0);
+
+    assert.equal(neighbors2.nw, null);
+    assert.equal(neighbors2.sw, null);
+    assert.equal(neighbors2.ne.x, 0);
+    assert.equal(neighbors2.ne.y, 0);
+    assert.equal(neighbors2.se.x, 0);
+    assert.equal(neighbors2.se.y, 1);
+  });
+
+  it('should be able to determine if vertex is an edge', function() {
+    var vertex1 = new Vertex(1, 1),
+        vertex2 = new Vertex(2, 2),
+        vertex3 = new Vertex(3, 2),
+        vertex4 = new Vertex(3, 3);
+
+    assert.equal(vertex1.checkIfEdge(sqrImgData), true);
+    assert.equal(vertex2.checkIfEdge(sqrImgData), true);
+    assert.equal(vertex3.checkIfEdge(sqrImgData), false);
+    assert.equal(vertex4.checkIfEdge(sqrImgData), false);
+  });
 
 });
